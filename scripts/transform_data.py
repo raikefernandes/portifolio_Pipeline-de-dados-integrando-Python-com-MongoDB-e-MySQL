9afba1990660f313b9cb0bb57864f42efd8d7eea
Raike Fernandes <raikericardovieirafernandes@gmail.com>
2025-03-14 22:18:27 -0300

    Adicionando transform_data.py e atualizações no pipeline

---
+++ scripts/transform_data.py
@@ -0,0 +1,39 @@
+from extract_and_save_data import connect_mongo, create_connect_db, create_connect_collection
+import pandas as pd
+
+def visualize_collection(col):
+    for doc in col.find():
+        print(doc)
+
+def rename_column(col, col_name, new_name):
+    col.update_many({}, {"$rename": {f"{col_name}": f"{new_name}"}})
+
+def select_category(col, category):
+    query = { "Categoria do Produto": f"{category}"}
+    
+    lista_categoria = []
+    for doc in col.find(query):
+        lista_categoria.append(doc)
+
+    return lista_categoria
+
+def make_regex(col, regex):
+    query = {"Data da Compra": {"$regex": f"{regex}"}}
+
+    lista_regex = []
+    for doc in col.find(query):
+        lista_regex.append(doc)
+    
+    return lista_regex
+
+def create_dataframe(lista):
+    df =  pd.DataFrame(lista)
+    return df
+
+def format_date(df):
+    df["Data da Compra"] = pd.to_datetime(df["Data da Compra"], format="%d/%m/%Y")
+    df['Data da Compra'] = df['Data da Compra'].dt.strftime('%Y-%m-%d')
+
+def save_csv(df, path):
+    df.to_csv(path, index=False)
+    print(f"\nO arquivo {path} foi salvo")
